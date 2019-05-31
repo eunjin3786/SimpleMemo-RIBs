248656fe3df47ce863049ec23ab72696187a6536
eunjin Jo <eunjin3786@naver.com>
2019-05-31 09:36:16 +0900

    로그인/회원가입 VC만들고 객체들 연결

---
+++ SimpleMemo/View/Controller/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  SimpleMemo
+//
+//  Created by eunjin Jo on 31/05/2019.
+//  Copyright © 2019 eunjin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ SimpleMemo/View/Controller/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  SimpleMemo
+//
+//  Created by eunjin Jo on 31/05/2019.
+//  Copyright © 2019 eunjin. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
