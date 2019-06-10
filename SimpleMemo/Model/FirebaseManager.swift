0156651b140eb7f5e08c93520634c235d1cd42be
eunjin Jo <eunjin3786@naver.com>
2019-06-10 23:35:00 +0900

    FirebaseManager 로그인 함수 추가

--- SimpleMemo/Model/FirebaseManager.swift
+++ SimpleMemo/Model/FirebaseManager.swift
@@ -60,4 +60,14 @@ extension FirebaseManager {
             }
         }
     }
+    
+    class func login(email: String, password: String, completion: @escaping (Result<AuthDataResult,Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if let result = result {
+                completion(.success(result))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
 }
