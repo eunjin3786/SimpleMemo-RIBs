9aef6c1369d11ca7f0c44108c683211b343c3e4e
jinny <jinny.jo@kakaocorp.com>
2020-01-08 15:50:43 +0900

    이것을 해줘야지 에러가 안남. lauchLouter를 변수로 가지고 있어야함.

--- SimpleMemo/App/AppDelegate.swift
+++ SimpleMemo/App/AppDelegate.swift
@@ -15,6 +15,7 @@ import RxSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    private var launchRouter: LaunchRouting?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -22,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = window
         
         let launchRouter = RootBuilder(dependency: AppComponent()).build()
+        self.launchRouter = launchRouter
         launchRouter.launchFromWindow(window)
 
         FirebaseApp.configure()
